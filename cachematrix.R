6e9950c79ad20b4885f03d10a7802b30a1a89425
clement <clement.tan.17@ucl.ac.uk>
2020-10-18 00:40:22 +0800

    Commit git commit -m Commit

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,51 @@
 ## functions do
 
 ## Write a short comment describing this function
+#The first function, makeCacheMatrix creates a list containing a function to
+
+#set the value of the matrix
+#get the value of the matrix
+#set the matrix of the inverse
+#get the matrix of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y){
+    x <<-y
+    m <<- NULL
+  }
+  get <- function() x 
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  
+  list(set = set, get = get, setinverse= setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
+#The following function calculates the inverse of the matrix set in the above function  
+#However, it first checks to see if the inverse has already been calculated. 
+#If so, it gets the inverse from the cache and skips the computation. 
+#Otherwise, it calculates the inverse of the matrix and sets the inverse in the cache via the solve function.
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message('getting cached data')
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  i
 }
+
+#testing
+test_matrix <- matrix(c(-3,5,1,0), nrow =2, ncol =2)
+?solve
+solve(test_matrix)
+t <- makeCacheMatrix(matrix(c(-3,5,1,0), nrow =2, ncol =2))
+t1 <- makeCacheMatrix(matrix(c(-3,6,1,0), nrow =2, ncol =2))
+cacheSolve(t)
+
